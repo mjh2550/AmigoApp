e1cd73164b0e5ae590771c6efc406f02e5e054a7
mjh2550 <moomjh94@naver.com>
2022-06-18 16:45:38 +0900

    .

--- app/src/main/java/com/android/amigo/MainActivity.kt
+++ app/src/main/java/com/android/amigo/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.android.amigo.data.test.TestVO
+import com.android.amigo.util.RetrofitManager
 import com.android.amigo.util.RetrofitService
 import retrofit2.Call
 import retrofit2.Callback
@@ -16,44 +17,37 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        retrofitRequest("192.168.219.105")
+        retrofitRequest("192.168.219.105","getAllList.do")
     }
 
-    private fun retrofitRequest(myIp: String) {
+    private fun retrofitRequest(myIp: String, request: String) {
 
-        val baseUrl = "http://$myIp:8090"
+        val call = RetrofitManager(myIp).call(request).apply {
+            enqueue(object : Callback<ArrayList<TestVO>> {
+                override fun onResponse(call: Call<ArrayList<TestVO>>, response: Response<ArrayList<TestVO>>) {
+                    if (response.isSuccessful) {
+                        var result = response.body()
 
-        val retrofit = Retrofit.Builder()
-            .baseUrl(baseUrl)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-
-        val service1 = retrofit.create(RetrofitService::class.java)
-
-        val call = service1.getPosts("getAllList.do")
-
-        call.enqueue(object : Callback<ArrayList<TestVO>> {
-            override fun onResponse(call: Call<ArrayList<TestVO>>, response: Response<ArrayList<TestVO>>) {
-                if (response.isSuccessful) {
-                    var result = response.body()
-
-                    if (result != null) {
-                        for(test in result){
-                            Log.d("Test","For : $test")
+                        if (result != null) {
+                            for(test in result){
+                                Log.d("Test","For : $test")
+                            }
                         }
+
+                        Log.d("Test", "OnRequest Success : $result")
+                    } else {
+                        Log.e("Test", "OnRequest Fail")
                     }
 
-                    Log.d("Test", "OnRequest Success : $result")
-                } else {
-                    Log.e("Test", "OnRequest Fail")
                 }
 
-            }
+                override fun onFailure(call: Call<ArrayList<TestVO>>, t: Throwable) {
+                    Log.d("Test", "On Fail : ${t.message}")
+                }
 
-            override fun onFailure(call: Call<ArrayList<TestVO>>, t: Throwable) {
-                Log.d("Test", "On Fail : ${t.message}")
-            }
+            })
+        }
 
-        })
+
     }
 }
--- app/src/main/java/com/android/amigo/util/RetrofitManager.kt
+++ app/src/main/java/com/android/amigo/util/RetrofitManager.kt
@@ -5,8 +5,26 @@ import retrofit2.Callback
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-object RetrofitManager {
+class RetrofitManager(myIp : String = "http://localhost:8090") {
 
+    private var baseUrl : String
 
+    init {
+        baseUrl = "http://${myIp}:8090"
+    }
+
+//   fun buildUrl(myIp : String) { baseUrl = "http://$myIp:8090"}
+
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .baseUrl(baseUrl)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+//    val service1 = retrofit.create(RetrofitService::class.java)
+
+    fun call(request: String): retrofit2.Call<ArrayList<TestVO>> {
+        return retrofit.create(RetrofitService::class.java).getPosts(request)
+    }
+//    val call = service1.getPosts("getAllList.do")
 
 }
